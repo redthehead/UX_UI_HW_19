f70045608a2279bc145702c5293167a656737fd5
redthehead <Carolynredford4@gmail.com>
2024-04-20 14:43:39 -0600

    Changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
 console.log("Your index.js file is loaded correctly");
 
-button.addEventListener('click', () => {
-    button.style.backgroundColor = '#f28705'
-});
+const hamNavBar = document.querySelector(".hamNavBar");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (hamNavBar.classList.contains("showMenu")) {
+    hamNavBar.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    hamNavBar.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
