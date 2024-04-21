92c56a62bbd0aa39b8977babd094a1d2978d61d0
redthehead <Carolynredford4@gmail.com>
2024-04-20 20:25:52 -0600

    changes

--- js/index.js
+++ js/index.js
@@ -25,29 +25,67 @@ hamburger.addEventListener("click", toggleMenu);
 //Hover states
 
 $(".resumeButton").mouseover(function() {
-  $(".resumeButton").css("background-color", "#032E3F").css("color","white");
+  $(".resumeButton").css("background-color", "#F28705").css("color","black");
 });
 $(".resumeButton").mouseout(function() {
   $(".resumeButton").css("background-color", "#f1c84a").css("color","black");
 });
 
 $(".emailButton").hover(function() {
-  $(".emailButton").css("background-color", "#032E3F").css("color","white"); 
+  $(".emailButton").css("background-color", "#F28705").css("color","black"); 
 });
 $(".emailButton").mouseout(function() {
   $(".emailButton").css("background-color", "#f1c84a").css("color","black");
 });
 
 $(".gitButton").mouseover(function() {
-  $(".gitButton").css("background-color", "#032E3F").css("color","white"); 
+  $(".gitButton").css("background-color", "#F28705")
 });
 $(".gitButton").mouseout(function() {
-  $(".gitButton").css("background-color", "#f1c84a").css("color","black");
+  $(".gitButton").css("background-color", "#f1c84a")
 });
 
 $(".linkedButton").mouseover(function() {
-  $(".linkedButton").css("background-color", "#032E3F");
+  $(".linkedButton").css("background-color", "#F28705");
 });
 $(".linkedButton").mouseout(function() {
-  $(".linkedButton").css("background-color", "#f1c84a").css("color","black");
+  $(".linkedButton").css("background-color", "#f1c84a")
+});
+
+$(".resumeIcon").mouseover(function() {
+  $(".resumeIcon").css("background-color", "#F28705");
+});
+$(".resumeIcon").mouseout(function() {
+  $(".resumeIcon").css("background-color", "#f1c84a")
+});
+
+$(".emailIcon").mouseover(function() {
+  $(".emailIcon").css("background-color", "#F28705");
+});
+$(".emailIcon").mouseout(function() {
+  $(".emailIcon").css("background-color", "#f1c84a")
+});
+
+$(".gitIcon").mouseover(function() {
+  $(".gitIcon").css("background-color", "#F28705");
+});
+$(".gitIcon").mouseout(function() {
+  $(".gitIcon").css("background-color", "#f1c84a")
+});
+
+$(".linkedIcon").mouseover(function() {
+  $(".linkedIcon").css("background-color", "#F28705");
+});
+$(".linkedIcon").mouseout(function() {
+  $(".linkedIcon").css("background-color", "#f1c84a")
+});
+
+$(".homeItemFoot").hover(function() {
+  $(".homeItemFoot").css("color", "#731702")
+});
+$(".aboutItemFoot").hover(function() {
+  $(".aboutItemFoot").css("color", "#731702")
+});
+$(".caseItemFoot").hover(function() {
+  $(".caseItemFoot").css("color", "#731702")
 });
